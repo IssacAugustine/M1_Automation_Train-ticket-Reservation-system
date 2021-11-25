1a12d5c9f8c0b4e76222c5db52bd67b7c2832156
IssacAugustine <94215887+IssacAugustine@users.noreply.github.com>
2021-11-25 08:55:28 +0530

    Update feature.h

--- 3_Implementation/inc/feature.h
+++ 3_Implementation/inc/feature.h
@@ -4,7 +4,10 @@
 #include<conio.h>
 
 
-
+char name[200];
+int age[200];
+int seatno[200];
+int seat[200];
 void booking(struct reserve a[200]);
 void cancellation(struct reserve a[200]); 
 void chart(struct reserve a[200]);
