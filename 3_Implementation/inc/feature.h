a5cc814ec1cb3fdd1e1937737a07ce2098413a45
IssacAugustine <94215887+IssacAugustine@users.noreply.github.com>
2021-11-25 08:51:16 +0530

    Update feature.h

--- 3_Implementation/inc/feature.h
+++ 3_Implementation/inc/feature.h
@@ -1,3 +1,16 @@
-void booking(struct reserve a[200])
-void cancellation(struct reserve a[200])
-void chart(struct reserve a[200])
+#ifndef __FUNCTION_H__
+#define __FUNCTION_H__
+#include<stdio.h>
+#include<conio.h>
+
+
+
+void booking(struct reserve a[200]);
+void cancellation(struct reserve a[200]); 
+void chart(struct reserve a[200]);
+
+
+
+
+
+#end if
