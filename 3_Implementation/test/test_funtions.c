0120b0da8818f2eb4a2e68c92e3caa82d147d905
IssacAugustine <94215887+IssacAugustine@users.noreply.github.com>
2021-11-25 12:10:35 +0530

    Update test_funtions.c

--- 3_Implementation/test/test_funtions.c
+++ 3_Implementation/test/test_funtions.c
@@ -3,3 +3,22 @@
 
 #define PROJECT_NAME  "TRAIN_TICKET_RESERVATION_SYSTEM"
 
+void test_booking(void){
+TEST_ASSERT_EQUAL(pass,booking(1));
+}
+void test_cancellation(void){
+TEST_ASSERT_EQUAL(pass,cancellation(1));
+}
+void test_chart(void){
+TEST_ASSERT_EQUAL(pass,chart());
+}
+int main()
+{
+UNITY_BEGIN();
+
+	RUN_TEST(test_booking);
+	RUN_TEST(test_cancellation);
+	RUN_TEST(test_chart);
+
+return UNITY_END();
+}
